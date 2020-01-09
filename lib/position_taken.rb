64560b7b4f850d4e858fe9c303440a545fef070d
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-09 20:42:34 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] = "" || board[index] = nil
     false
-  else board[index] == "X" || "O"
-    false
+  else board[index] == "X" || board[index] == "O"
+    true
   end
 end
